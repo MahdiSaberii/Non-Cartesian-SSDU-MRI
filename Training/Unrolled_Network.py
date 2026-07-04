b429390093b46766468a09905eb105d97fb07ae6
Mahdi Saberi <129208885+MahdiSaberii@users.noreply.github.com>
2026-07-04 02:05:20 -0500

    Remove unnecessary reshaping of recon tensor

--- Training/Unrolled_Network.py
+++ Training/Unrolled_Network.py
@@ -27,10 +27,7 @@ class UnrolledNet(torch.nn.Module):
         
         for OuterIter in range(self.Unrolls):
             recon = self.complex2real(recon)  # [B, E, 2, H, W]
-            B, E, C, H, W = recon.shape
-            recon = recon.reshape(B, E * C, H, W)  # [B, 2E, H, W]
             recon = self.Network(recon.float())    # [B, 2E, H, W]
-            recon = recon.reshape(B, E, C, H, W)   # [B, E, 2, H, W]
             recon = self.real2complex(recon)       # [B, E, H, W]
             recon = self.DataConsistency(recon, zf, coil_map, M)
         return recon
@@ -61,10 +58,7 @@ class UnrolledNet_PE(torch.nn.Module):
         for OuterIter in range(self.Unrolls):
             recon = recon * torch.conj(p_prime) # Phase Corection
             recon = self.complex2real(recon)       # [B, E, 2, H, W]
-            B, E, C, H, W = recon.shape
-            recon = recon.reshape(B, E * C, H, W)  # [B, 2E, H, W]
             recon = self.Network(recon.float()) # [B, 2E, H, W]
-            recon = recon.reshape(B, E, C, H, W)   # [B, E, 2, H, W]
             recon = self.real2complex(recon)       # [B, E, H, W]
             recon = recon * p_prime                # Phase Corection
             recon = self.DataConsistency(recon, zf, coil_map, M)

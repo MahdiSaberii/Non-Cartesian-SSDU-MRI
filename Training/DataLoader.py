e30ffa02601dc70c608912b78897dc86457f262b
Mahdi Saberi <129208885+MahdiSaberii@users.noreply.github.com>
2026-07-03 23:46:10 -0500

    Refactor DataLoaderSL to use new preprocess paths
    Updated import paths for data files and corrected the directory structure for loading coil and M operator data.

--- Training/DataLoader.py
+++ Training/DataLoader.py
@@ -34,17 +34,17 @@ class DataLoaderSL(Dataset):
       TimeIndex       = int(FileName.split('_')[-1].split('.')[0])
       # Split takes: subject_1_slice_1_t_55 and gives: ['subject', '1', 'slice', '1', 't', '55']
 
-      CoilAddress = f"../Data/Coils/{SubjectNumber}/slice_{SliceNumber}.mat"
+      CoilAddress = f"../Preprocess/Coils/{SubjectNumber}/slice_{SliceNumber}.mat"
       Coil        = sio.loadmat(CoilAddress)["sens_map"].transpose([2,0,1])   # Original Coil shape: [120,120,44] -> [44,120,120]
       
       
       # M Operator
-      M_single = sio.loadmat(f"../Data/M/M_single.mat")["M"] # [14,5,240,240] == [2*Masks, Echoes, 2*img_width, 2*img_width]
-      M_full   = sio.loadmat(f"../Data/M/M_full.mat")["M"]   # [5,240,240]    == [Echoes,          2*img_width, 2*img_width]
+      M_single = sio.loadmat(f"../Preprocess/M/M_single.mat")["M"] # [14,5,240,240] == [2*Masks, Echoes, 2*img_width, 2*img_width]
+      M_full   = sio.loadmat(f"../Preprocess/M/M_full.mat")["M"]   # [5,240,240]    == [Echoes,          2*img_width, 2*img_width]
       M_single = torch.tensor(M_single)
       M_full   = torch.tensor(M_full)
       
-      label_path = f"../Data/Labels/"+ "subject_" + str(SubjectNumber) + "_slice_"+str(SliceNumber) + ".mat"
+      label_path = f"../Preprocess/Labels/"+ "subject_" + str(SubjectNumber) + "_slice_"+str(SliceNumber) + ".mat"
       label_ehy  = sio.loadmat(label_path)[f"label_ehy_temp"].transpose([2,0,1])  # Original Data shape: [120,120,5] 
       
       return Theta_EHWy, Lambda_EHWy, Coil , SliceNumber , SubjectNumber, TimeIndex, FileName, M_single,label_ehy, M_full
